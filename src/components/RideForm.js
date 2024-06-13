0e5e10370b329a3aafe4471c49e65325d4ea90c2
Yingying Wang <yxw241@gmail.com>
2024-06-13 18:49:45 -0400

    update on RideForm.js for creating the ride

--- src/components/RideForm.js
+++ src/components/RideForm.js
@@ -6,6 +6,7 @@ const RideForm = () => {
   const [destination, setDestination] = useState('');
   const [pickupTime, setPickupTime] = useState('');
   const [seatsAvailable, setSeatsAvailable] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -16,48 +17,57 @@ const RideForm = () => {
       pickupTime,
       seatsAvailable
     });
+    setSuccessMessage('Ride created successfully!');
+    // Clear the form fields
+    setPickupLocation('');
+    setDestination('');
+    setPickupTime('');
+    setSeatsAvailable('');
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Pickup Location:</label>
-        <input
-          type="text"
-          value={pickupLocation}
-          onChange={(e) => setPickupLocation(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Destination:</label>
-        <input
-          type="text"
-          value={destination}
-          onChange={(e) => setDestination(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Pickup Time:</label>
-        <input
-          type="datetime-local"
-          value={pickupTime}
-          onChange={(e) => setPickupTime(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label>Seats Available:</label>
-        <input
-          type="number"
-          value={seatsAvailable}
-          onChange={(e) => setSeatsAvailable(e.target.value)}
-          required
-        />
-      </div>
-      <button type="submit">Create Ride</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Pickup Location:</label>
+          <input
+            type="text"
+            value={pickupLocation}
+            onChange={(e) => setPickupLocation(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Destination:</label>
+          <input
+            type="text"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Pickup Time:</label>
+          <input
+            type="datetime-local"
+            value={pickupTime}
+            onChange={(e) => setPickupTime(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Seats Available:</label>
+          <input
+            type="number"
+            value={seatsAvailable}
+            onChange={(e) => setSeatsAvailable(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit">Create Ride</button>
+      </form>
+      {successMessage && <p>{successMessage}</p>}
+    </div>
   );
 };
 
