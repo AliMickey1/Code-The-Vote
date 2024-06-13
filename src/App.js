e3349bcb38e252c465c9545e3dd8eec95fade5b0
Aliceson Phillips <alicesonphillips@gmail.com>
2024-06-13 18:54:57 -0400

    Authentication start waiting for push of new homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+// import LoginButton from './Components/LoginButton';
+// import LogoutButton from './Components/LogoutButton';
 
 function App() {
   return (
