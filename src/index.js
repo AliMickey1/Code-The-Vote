22c9f7278b5b3f4f96c71a85212491d2911769f4
Logan <thelogan2019@gmail.com>
2024-06-15 11:08:14 -0500

    Attempted to get auth working for all users

--- src/index.js
+++ src/index.js
@@ -3,28 +3,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
 import "./index.css";
 import AppRouter from "../src/common/AppRouter";
-import { Auth0Provider } from '@auth0/auth0-react';
+import Auth0ProviderWithHistory from "./common/auth0-provider-with-history";
 
 
-
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0Provider
-        domain={domain}
-        clientId={clientId}
-        authorizationParams={{
-          redirect_uri: window.location.origin}}
-      >
-      <AppRouter />
-    </Auth0Provider>
-  </BrowserRouter>
+      <Auth0ProviderWithHistory>
+        <AppRouter />
+      </Auth0ProviderWithHistory>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
-
-
