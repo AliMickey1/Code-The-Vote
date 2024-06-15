55933a18dfa9337559ccdf71de123e9b2f091d19
Logan <thelogan2019@gmail.com>
2024-06-15 17:28:23 -0500

    styled wrong page

--- src/pages/NoPageFound.js
+++ src/pages/NoPageFound.js
@@ -1,7 +1,15 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
 function NoPageFound() {
+  const location = useLocation();
+  const address = location.pathname.substring(1);
+
   return (
-    <div className="currentPage">
-      <p>NO PAGE FOUND</p>
+    <div>
+      <h1 className="wrongPageBackground">
+        Sorry, the page&nbsp;<em>{address}</em>&nbsp;does not exist.
+      </h1>
     </div>
   );
 }
